c5f54273823572308b7bd1eb31a9d690aa5d3a87
edoardogiammarco <edoardo.giammarco@gmail.com>
2025-05-15 23:49:05 +0200

    Add movie-showcase-backend/src/main/java/com/example/movieshowcase/repository/UserRepository.java 1747345745123

---
+++ movie-showcase-backend/src/main/java/com/example/movieshowcase/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.movieshowcase.repository;
+
+import com.example.movieshowcase.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
