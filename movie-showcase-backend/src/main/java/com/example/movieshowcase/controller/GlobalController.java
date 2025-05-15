232a2fe14256828a30aa69eac61ffcaffeaa0549
edoardogiammarco <edoardo.giammarco@gmail.com>
2025-05-15 23:48:58 +0200

    Add movie-showcase-backend/src/main/java/com/example/movieshowcase/controller/GlobalController.java 1747345737980

---
+++ movie-showcase-backend/src/main/java/com/example/movieshowcase/controller/GlobalController.java
@@ -0,0 +1,13 @@
+package com.example.movieshowcase.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GlobalController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the Movie Showcase API";
+    }
+}
